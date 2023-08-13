4b2d544b8353fd73656141e9974c1fcc99add9df
Serik <tomshooter1106@gmail.com>
2023-08-13 22:19:24 +0600

    create section seeder

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Section;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class SectionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $sections = Section::factory(10)->create();
     }
 }
